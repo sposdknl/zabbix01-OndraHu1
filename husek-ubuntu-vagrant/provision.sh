a02fc6abf388a62db02b82f0771f58c2043efa50
Hušek Ondřej <husek.ondra1@seznam.cz>
2025-01-10 09:26:06 +0100

    vse hotovo :)

--- husek-ubuntu-vagrant/provision.sh
+++ husek-ubuntu-vagrant/provision.sh
@@ -1,52 +1,36 @@
 #!/bin/bash
 
-# Aktualizace systému
-sudo dnf update -y
-
-# Instalace základních nástrojů
-sudo dnf install -y wget curl vim gnupg2
-
-# Instalace MySQL serveru (MariaDB)
-sudo dnf install -y mariadb-server
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y wget curl vim gnupg
+sudo apt install -y mariadb-server
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 
-# Konfigurace databáze pro Zabbix
 sudo mysql -e "CREATE DATABASE zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
 sudo mysql -e "CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'zabbix_password';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost';"
 sudo mysql -e "SET GLOBAL log_bin_trust_function_creators = 1;"
 sudo mysql -e "FLUSH PRIVILEGES;"
 
-# Přidání Zabbix 7.0 LTS repository
-wget https://repo.zabbix.com/zabbix/7.0/rhel/9/x86_64/zabbix-release-7.0-1.el9.noarch.rpm
-sudo dnf install -y zabbix-release-7.0-1.el9.noarch.rpm
-sudo dnf clean all
-sudo dnf makecache
+wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix-release_7.0-1+debian12_all.deb
+sudo dpkg -i zabbix-release_7.0-1+debian12_all.deb
+sudo apt update -y
 
-# Instalace Zabbix serveru, frontend, Apache a PHP
-sudo dnf install -y zabbix-server-mysql zabbix-apache-conf zabbix-sql-scripts zabbix-agent2
-sudo dnf install -y httpd php php-mbstring php-gd php-xml php-bcmath
+sudo apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent2
+sudo apt install -y apache2 php php-mbstring php-gd php-xml php-bcmath
 
-# Import Zabbix databázové struktury
 sudo zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -pzabbix_password zabbix
-
-# Disable log_bin_trust_function_creators option after importing database schema.
 sudo mysql -e "SET GLOBAL log_bin_trust_function_creators = 0;"
 
-# Konfigurace Zabbix serveru
 sudo sed -i 's/# DBPassword=/DBPassword=zabbix_password/' /etc/zabbix/zabbix_server.conf
 
-# Spuštění Zabbix serveru a agenta
-sudo systemctl restart zabbix-server zabbix-agent2 httpd
-sudo systemctl enable zabbix-server zabbix-agent2 httpd
+sudo systemctl restart zabbix-server zabbix-agent2 apache2
+sudo systemctl enable zabbix-server zabbix-agent2 apache2
 
-# Konfigurace PHP pro Zabbix frontend
-sudo sed -i 's/^max_execution_time = .*/max_execution_time = 300/' /etc/php.ini
-sudo sed -i 's/^memory_limit = .*/memory_limit = 128M/' /etc/php.ini
-sudo sed -i 's/^post_max_size = .*/post_max_size = 16M/' /etc/php.ini
-sudo sed -i 's/^upload_max_filesize = .*/upload_max_filesize = 2M/' /etc/php.ini
-sudo sed -i 's/^;date.timezone =.*/date.timezone = Europe\/Prague/' /etc/php.ini
+sudo sed -i 's/^max_execution_time = .*/max_execution_time = 300/' /etc/php/7.4/apache2/php.ini
+sudo sed -i 's/^memory_limit = .*/memory_limit = 128M/' /etc/php/7.4/apache2/php.ini
+sudo sed -i 's/^post_max_size = .*/post_max_size = 16M/' /etc/php/7.4/apache2/php.ini
+sudo sed -i 's/^upload_max_filesize = .*/upload_max_filesize = 2M/' /etc/php/7.4/apache2/php.ini
+sudo sed -i 's/^;date.timezone =.*/date.timezone = Europe\/Prague/' /etc/php/7.4/apache2/php.ini
 
-# Restart Apache pro načtení změn
-sudo systemctl restart httpd
+sudo systemctl restart apache2
